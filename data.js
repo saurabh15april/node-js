429f3e330535997dd99874fbb439f5b9ec0ce20b
Saurabh Dubey <dubesaurabh99@gmail.com>
2024-03-25 12:27:22 +0530

    http server & fileSystem fs created in index.js file

---
+++ data.js
@@ -0,0 +1,8 @@
+const data =[ {name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+{name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+{name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+{name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+{name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+{name: " saurabh" , email: 'saurabh@Test.com', password: '123456789'},
+]
+module.exports = data ;
--- index.js
+++ index.js
@@ -7,10 +7,40 @@
 
 
 // const proxy = http.createServer(dataHandle).listen(4600);
-const colors = require('colors');
+// const colors = require('colors');
 
-console.log('hello'.green); // outputs green text
-console.log('i like cake and pies'.underline.red) // outputs red underlined text
-console.log('inverse the color'.inverse); // inverses the color
-console.log('OMG Rainbows!'.rainbow); // rainbow
-console.log('Run the trap'.trap); // Drops the bass
+// console.log('hello'.green); // outputs green text
+// console.log('i like cake and pies'.underline.red) // outputs red underlined text
+// console.log('inverse the color'.inverse); // inverses the color
+// console.log('OMG Rainbows!'.rainbow); // rainbow
+// console.log('Run the trap'.trap); // Drops the bass
+
+// server setup 
+                                     
+// const  http = require('http');       // create http server 
+// const data = require('./data');
+//                                     //create a server object:
+// http.createServer(function (req, res) {
+//     res.writeHead(404 );
+//   res.write(JSON.stringify(data)); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+
+
+// file manage system 
+// const fs = require('fs');
+// const input = process.argv;
+// if (input[2]=="add"){
+//     fs.writeFileSync(input[3], input[4]);
+// }else if(input[2]== "remove"){
+//     fs.unlinkSync(input[3]);
+
+// }else{
+//     console.log("invalid entry")
+// }
+
+// console.log(input);
+
+
+
